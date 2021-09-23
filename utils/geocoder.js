8fd3f3a3199291956ee9f787ed78c83c84b0f68b
N Liraj Khanna <lirajkhanna2002@gmail.com>
2021-09-23 20:02:35 +0530

    23-09-2021 20:01 Committed

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -3,7 +3,7 @@ const NodeGeocoder = require('node-geocoder');
 const options = {
   provider: process.env.GEOCODER_PROVIDER || "mapquest",
   httpAdapter: 'https',
-  apiKey: process.env.GEOCODER_API_KEY || "YSfK1TEF4fRTXQnkvSc1Z6iHrGaX3Vej",
+  apiKey: process.env.GEOCODER_API_KEY,
   formatter: null
 };
 const geocoder = NodeGeocoder(options);
